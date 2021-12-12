c036fe98d1540861e33816f6e2c33e17b88782ae
opheus2 <j.ominiabohs@gmail.com>
2021-12-12 20:55:19 +0800

    implement monoclient

---
+++ app/Services/Mono/MonoClient.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace App\Services\Mono;
+
+use GuzzleHttp\Client;
+use App\Services\HttpResponseModel;
+use GuzzleHttp\Exception\GuzzleException;
+
+class MonoClient
+{
+    const METHODS = ['get', 'post', 'put', 'patch', 'delete', 'head'];
+    const DEFAULT_METHOD = 'get';
+    const SUCCESS_CODES = [200, 201, 202];
+
+    private Client $client;
+    private array $_body;
+    private array $_headers;
+    private string $_method;
+    private string $_uri;
+    private string $_baseUri;
+    private string $_query;
+
+    private string $apiSecret;
+
+    public function __construct()
+    {
+        $this->apiSecret = config('services.mono.secret');
+        $this->_baseUri = config('services.mono.api');
+        $this->_method = 'GET';
+        $this->_headers = [
+            'Accept' => ' application/json',
+            'mono-sec-key' => $this->apiSecret
+        ];
+        $this->client = new Client();
+        $this->_body = [];
+        $this->_query = "";
+    }
+
+    private function body(array $body)
+    {
+        $this->_body = array_merge($this->_body, $body);
+        return $this;
+    }
+
+    private function query(array $query)
+    {
+        $parseQuery = $this->_method == 'get' ? array_merge(['access_key' => $this->apiToken], $query) : $query;
+        $this->_query = http_build_query($parseQuery);
+        return $this;
+    }
+
+    private function headers(array $headers)
+    {
+        $this->_headers = array_merge($this->_headers, $headers);
+        return $this;
+    }
+
+    private function uri($uri)
+    {
+        $this->_uri = $this->_baseUri . stripslashes(trim(rtrim($uri)));
+        return $this;
+    }
+
+    private function method(string $method)
+    {
+        $this->_method = in_array(strtolower($method), self::METHODS)
+            ? $method
+            : self::DEFAULT_METHOD;
+
+        return $this;
+    }
+
+    private function reportException(\Exception $e)
+    {
+        $this->reportError($e->getMessage(), $e->getLine());
+    }
+
+    private function reportError($message, $line)
+    {
+        // Handle Error
+    }
+
+    private function send(): HttpResponseModel
+    {
+        $_config = [];
+        $_config['form_params'] = $this->_body;
+        $_config['headers'] = $this->_headers;
+
+        $status = false;
+        $statusCode = 400;
+        $data = [];
+        $message = '';
+        $error = '';
+        $errors = [];
+        if ($this->_query == "") {
+            $this->query([]);
+        }
+        $this->_uri = "{$this->_uri}?{$this->_query}";
+        // dd($this->client->request(
+        //     $this->_method,
+        //     $this->_uri,
+        //     $_config
+        // ));
+
+        try {
+            $response = $this->client->request(
+                $this->_method,
+                $this->_uri,
+                $_config
+            );
+
+            $statusCode = $response->getStatusCode();
+
+            if (in_array($statusCode, self::SUCCESS_CODES)) {
+                $status = true;
+                $data = json_decode($response->getBody());
+            }
+        } catch (GuzzleException $e) {
+            $status = false;
+            $response = $e->getResponse()->getBody();
+            $statusCode = $e->getCode();
+            $body = json_decode($response, true);
+            $message = $body['message'];
+            $errors = $body;
+        }
+
+        return new HttpResponseModel(
+            $status,
+            $statusCode,
+            $data,
+            $message,
+            $error,
+            $errors,
+        );
+    }
+
+    public function getExchangeToken(string $code): HttpResponseModel
+    {
+        $response = $this
+            ->method('POST')
+            ->uri('/account/auth')
+            ->body([
+                'code' => $code
+            ])
+            ->send();
+
+        return $response;
+    }
+
+    public function getAccount(string $account): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/accounts/{$account}")
+            ->send();
+
+        return $response;
+    }
+
+    public function getTransactions(string $account): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/accounts/{$account}/transactions")
+            ->send();
+
+        return $response;
+    }
+
+    public function getStatements(string $account, string $period = "last6months"): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/accounts/{$account}/statement")
+            ->query(["period" => $period])
+            ->send();
+
+        return $response;
+    }
+
+    public function getCreditHistory(string $account): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/accounts/{$account}/credits")
+            ->send();
+
+        return $response;
+    }
+
+    public function getDebitHistory(string $account): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/accounts/{$account}/debits")
+            ->send();
+
+        return $response;
+    }
+
+    public function getAverageIncome(string $account): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/accounts/{$account}/income")
+            ->send();
+
+        return $response;
+    }
+
+    public function getIdentity(string $account): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/accounts/{$account}/identity")
+            ->send();
+
+        return $response;
+    }
+
+    public function syncData(string $account): HttpResponseModel
+    {
+        $response = $this
+            ->method('POST')
+            ->uri("/accounts/{$account}/sync")
+            ->send();
+
+        if ($response->data->status === "failed") {
+            return new HttpResponseModel(
+                false,
+                400,
+                json_decode(json_encode($response->data), true),
+                $response->data->message,
+                '',
+            );
+        }
+
+        return $response;
+    }
+
+    public function getWalletBalance(): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/users/stats/wallet")
+            ->send();
+
+        return $response;
+    }
+
+    public function getInstitutions(): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/coverage")
+            ->send();
+
+        return $response;
+    }
+
+    public function lookupBusiness(string $businessId): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/v1/cac/lookup")
+            ->query([
+                "name" => $businessId
+            ])
+            ->send();
+
+        return $response;
+    }
+
+    public function lookupShareholders(string $companyId): HttpResponseModel
+    {
+        $response = $this
+            ->method('GET')
+            ->uri("/v1/cac/company/{$companyId}")
+            ->send();
+
+        return $response;
+    }
+}
--- routes/api.v1.php
+++ routes/api.v1.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\Auth\EmailVerificationController;
 use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\LogoutController;
 use App\Http\Controllers\API\Auth\RegisterController;
+use App\Services\Mono\MonoClient;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -29,4 +30,23 @@ Route::prefix('auth')->name('auth.')->group(function () {
     Route::post('logout', LogoutController::class)->name('user.logout');
     Route::post('email', [EmailVerificationController::class, 'sendEmailToken'])->middleware(['throttle:email_verification'])->name('email.token');
     Route::post('email/verify', [EmailVerificationController::class, 'verifyEmail'])->name('email.verify');
+});
+
+Route::post('/testMono', function (Request $request, MonoClient $monoClient) {
+    // $data = $monoClient->getExchangeToken($request->code);
+    // $data = $monoClient->getAccount($request->account);
+    // $data = $monoClient->getTransactions($request->account);
+    // $data = $monoClient->getStatements($request->account, "last6months");
+    // $data = $monoClient->getCreditHistory($request->account);
+    // $data = $monoClient->getDebitHistory($request->account);
+    // $data = $monoClient->getAverageIncome($request->account);
+    // $data = $monoClient->getIdentity($request->account);
+    // $data = $monoClient->syncData($request->account);
+    // $data = $monoClient->getWalletBalance();
+    // $data = $monoClient->getInstitutions();
+    // $data = $monoClient->lookupBusiness($request->business_name);
+    // $data = $monoClient->lookupShareholders($request->company_id);
+
+    // return $data;
+    // dd($data);
 });
